d4f6d542df35d435f4b9a051446fce4731f66d63
Draweshwarkumar <shivamjha22948@gmail.com>
2024-10-09 19:52:44 +0530

    successfully generated token

---
+++ src/main/java/com/jwt/example/models/JwtResponse.java
@@ -0,0 +1,51 @@
+package com.jwt.example.models;
+
+public class JwtResponse {
+    
+    private String jwtToken;
+    private String username;
+
+    // Getters and setters
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Builder pattern
+    public static JwtResponseBuilder builder() {
+        return new JwtResponseBuilder();
+    }
+
+    public static class JwtResponseBuilder {
+        private String jwtToken;
+        private String username;
+
+        public JwtResponseBuilder jwtToken(String jwtToken) {
+            this.jwtToken = jwtToken;
+            return this;
+        }
+
+        public JwtResponseBuilder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public JwtResponse build() {
+            JwtResponse jwtResponse = new JwtResponse();
+            jwtResponse.setJwtToken(this.jwtToken);
+            jwtResponse.setUsername(this.username);
+            return jwtResponse;
+        }
+    }
+}
