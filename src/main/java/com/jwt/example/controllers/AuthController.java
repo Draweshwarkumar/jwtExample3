44fcdd8c8475e7f5c7e6d706a81c0fc8e0deeb6b
Draweshwarkumar <shivamjha22948@gmail.com>
2024-12-25 22:45:18 +0530

    Update AuthController.java

--- src/main/java/com/jwt/example/controllers/AuthController.java
+++ src/main/java/com/jwt/example/controllers/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ import ch.qos.logback.classic.Logger;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 public class AuthController {
 
     @Autowired
